221f2cca3dfdeaf9044d8790ee224041f7bd0ec5
Angela-GM <angelagm0112@gmail.com>
2023-07-05 14:57:15 +0200

    completa

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { RiCloseCircleLine } from 'react-icons/ri'
+import { TiEdit } from 'react-icons/ti'
+import { ToDoForm } from "./ToDoForm";
+
+export interface ToDo {
+  id: string,
+  text: string,
+}
+
+type Props = {
+  toDos: Array<ToDo>,
+  removeToDo: (id:any) => void,
+  updateToDo: () => void,
+}
+
+
+
+export const ToDo = ({ toDos, removeToDo, updateToDo }: Props) => {
+  const [ edit, setEdit ] = useState<ToDo | unknown>( { id: '', text: '' } );
+
+  const submitUpdate = (value) => {
+    updateToDo(edit.id, value);
+    setEdit({
+      id: '',
+      value: ''
+    })
+  }
+
+  if (edit.id) {
+    return <ToDoForm edit={edit} onSubmit={submitUpdate} />
+  }
+
+  return toDos.map((toDo: ToDo, index ) => 
+  <div className="toDo-row" key={index}>
+
+    <div key={toDo.id}>
+      {toDo.text}
+    </div>
+
+    <div className="icons">
+      <RiCloseCircleLine onClick={() => removeToDo(toDo.id)} className='delete-icon' />
+      <TiEdit onClick={() => setEdit({ id: toDo.id, value: toDo.text })} />
+    </div>
+
+
+  </div>  
+  )
+
+}
---
+++ src/components/ToDoForm.tsx
@@ -0,0 +1,54 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { v4 as uuid } from 'uuid';
+import { ToDo } from './ToDo';
+type Props = {
+    onSubmit: (toDo:ToDo) => void,
+};
+export const ToDoForm = ( props: Props ) => {
+    const [ input, setInput ]= useState<string>(props.edit ? props.edit.value : '');
+    
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        props.onSubmit(
+            {
+                id: uuid(),
+                text: input,
+            }
+        )
+        setInput('');
+
+    }
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInput(e.target.value)
+    }
+
+    
+
+
+  return (
+    <form onSubmit={handleSubmit} className='toDo-form' aria-labelledby='toDo-form' >
+
+        {props.edit ? (<>
+        
+            <input onChange={handleChange} type="text"  placeholder='Update your item' value={input} name='text' className='toDo-input edit' />
+            <button type='submit' className='toDo-button edit'>Update</button>
+            </>
+
+        ): (
+            <>
+            <input onChange={handleChange} type="text"  placeholder='Add a to do' value={input} name='text' className='toDo-input' />
+            <button type='submit' className='toDo-button'>Add something to do</button>
+            
+            </>
+
+
+        )}
+        
+
+
+
+    </form>
+    
+  )
+}
+
